0ce922869bfa106c31f5fb1852768fb71a564f6a
Bram Veenboer <bram.veenboer@gmail.com>
2025-09-03 14:33:36 +0200

    IDG-CPP: fixes to the interface of some helper functions

--- idg/cpp/include/init.h
+++ idg/cpp/include/init.h
@@ -15,16 +15,15 @@ xt::xarray<float> get_frequencies(const float start_frequency,
 std::vector<Metadata> get_metadata(const size_t nr_channels,
                                    const size_t subgrid_size,
                                    const size_t grid_size,
-                                   const xt::xarray<float> &uvw,
+                                   const xt::xarray<UVW> &uvw,
                                    const size_t max_group_size = 256);
 
 xt::xarray<VisibilityType>
 get_visibilities(const size_t nr_correlations, const size_t nr_channels,
                  const size_t nr_timesteps, const size_t nr_baselines,
                  const float image_size, const size_t grid_size,
-                 const xt::xarray<double> &frequencies,
-                 const xt::xarray<float> &uvw,
-                 const size_t nr_point_sources = 4, int max_pixel_offset = -1,
-                 const int random_seed = 2);
+                 const xt::xarray<float> &frequencies,
+                 const xt::xarray<UVW> &uvw, const size_t nr_point_sources = 4,
+                 int max_pixel_offset = -1, const int random_seed = 2);
 
 xt::xarray<float> get_taper(const size_t subgrid_size);
