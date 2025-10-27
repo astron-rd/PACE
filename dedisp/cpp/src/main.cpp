06a983909f027d94621378a84e33cc92ce824db1
Mick Veldhuis <veldhuis@astron.nl>
2025-10-27 10:46:01 +0100

    Add TODO to CLI entrypoint

--- dedisp/cpp/src/main.cpp
+++ dedisp/cpp/src/main.cpp
@@ -6,75 +6,8 @@
 #include "metadata.hpp"
 
 int main() {
-  // Observation details: duration, integration time, max. frequency, bandwidth,
-  // and channel count.
-  const dedisp::ObservationInfo obs_info{30.0f, 250.0e-6, 1581.0f, 100.0f,
-                                         1024};
+    std::cout << "PACE - Fourier Domain Dedispersion" << std::endl;
 
-  // Mock signal parameters: RMS noise floor, DM, pulse arrival time, and signal
-  // amplitude.
-  const dedisp::SignalInfo mock_signal{25.0f, 41.159f, 3.14159f, 25.0f};
-
-  // Dedispersion plan constraints: start DM, end DM, pulse width (ms), smearing
-  // tolerance.
-  const dedisp::DedispersionConstraints constraints{2.0f, 100.0f, 4.0f, 1.25f};
-
-  const float sampling_period =
-      250.0E-6; // Base is 250 microsecond time samples
-  const float down_sampling_ratio = 1.0;
-  const float observation_duration = 30.0; // Observation duration in seconds
-  const float time_resolution =
-      down_sampling_ratio * sampling_period; // s (0.25 ms sampling)
-  const float peak_frequency = 1581.0;       // MHz (highest channel!)
-  const float bandwidth = 100.0;             // MHz
-  const size_t n_channels = 1024;
-  const float frequency_resolution =
-      -1.0 * bandwidth / n_channels; // MHz   (This must be negative!)
-
-  const size_t n_samples = observation_duration / time_resolution;
-  const float data_rms = 25.0;
-  const float signal_dm = 41.159;
-  const float signal_time = 3.14159;   // seconds into time series (at f0)
-  const float signal_amplitude = 25.0; // amplitude of signal
-
-  const float dm_start = 2.0;    // pc cm^-3
-  const float dm_end = 100.0;    // pc cm^-3
-  const float pulse_width = 4.0; // ms
-  const float dm_tolerance = 1.25;
-  const size_t n_bits_in = 8;
-  const size_t n_bits_out = 32;
-
-  size_t dm_count;
-  size_t max_delay;
-  size_t n_samples_computed;
-  unsigned char *input = 0;
-  unsigned char *output = 0;
-
-  // Initialise and execute the FDD plan
-  std::cout << "Initialise FDD Plan <--" << std::endl;
-  auto start = std::chrono::high_resolution_clock::now();
-  dedisp::FDDPlan fdd_plan(n_channels, time_resolution, peak_frequency,
-                           frequency_resolution);
-  auto end = std::chrono::high_resolution_clock::now();
-  std::cout << "--> runtime: "
-            << std::chrono::duration<double>(end - start).count() << " seconds"
-            << std::endl;
-
-  std::cout << "Generate DM list <--" << std::endl;
-  start = std::chrono::high_resolution_clock::now();
-  fdd_plan.generate_dm_list(dm_start, dm_end, pulse_width, dm_tolerance);
-  end = std::chrono::high_resolution_clock::now();
-  std::cout << "--> runtime: "
-            << std::chrono::duration<double>(end - start).count() << " seconds"
-            << std::endl;
-
-  std::cout << "Execute FDD Plan <--" << std::endl;
-  start = std::chrono::high_resolution_clock::now();
-  fdd_plan.execute(n_samples, input, n_bits_in, output, n_bits_out);
-  end = std::chrono::high_resolution_clock::now();
-  std::cout << "--> runtime: "
-            << std::chrono::duration<double>(end - start).count() << " seconds"
-            << std::endl;
-
-  fdd_plan.show();
+    // TODO: implement command-line interface.
+    // IDEA: allow input as npy file and deserialise using xtensor; provide additional arguments for metadata.
 }
