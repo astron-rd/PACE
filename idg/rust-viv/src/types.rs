3f34e8e79c554b86c476ce9771fc6ffd9f5e1c7b
Vivian <me@vivstuff.gay>
2026-02-02 13:28:37 +0100

    Add deserialization for metadata

--- idg/rust-viv/src/types.rs
+++ idg/rust-viv/src/types.rs
@@ -8,7 +8,7 @@ use ndarray::Array2;
 use num_traits::identities::Zero;
 
 /// 3-dimensional vector with UVW parameters
-#[derive(Clone, Copy, Debug)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Uvw {
     pub u: f32,
     pub v: f32,
@@ -53,20 +53,14 @@ impl ReadableElement for Uvw {
         type_desc: &py_literal::Value,
         len: usize,
     ) -> Result<Vec<Self>, ReadDataError> {
-        check_type_desc(type_desc)?;
-        
+        check_type_desc(type_desc, "[('u', '<f4'), ('v', '<f4'), ('w', '<f4')]")?;
+
         let mut out = Vec::with_capacity(len);
 
         for _ in 0..len {
-            let u = reader
-                .read_f32::<LittleEndian>()
-                .map_err(|_| ReadDataError::MissingData)?;
-            let v = reader
-                .read_f32::<LittleEndian>()
-                .map_err(|_| ReadDataError::MissingData)?;
-            let w = reader
-                .read_f32::<LittleEndian>()
-                .map_err(|_| ReadDataError::MissingData)?;
+            let u = reader.read_f32::<LittleEndian>()?;
+            let v = reader.read_f32::<LittleEndian>()?;
+            let w = reader.read_f32::<LittleEndian>()?;
 
             out.push(Uvw::new(u, v, w));
         }
@@ -77,12 +71,12 @@ impl ReadableElement for Uvw {
     }
 }
 
-fn check_type_desc(type_desc: &Value) -> Result<(), ReadDataError> {
+fn check_type_desc(type_desc: &Value, expected: &str) -> Result<(), ReadDataError> {
     // Is this cheating? Potentially.
     // Does it really matter in this context? I don't think so.
     let signature = format!("{}", type_desc);
 
-    if signature == "[('u', '<f4'), ('v', '<f4'), ('w', '<f4')]" {
+    if signature == expected {
         Ok(())
     } else {
         Err(ReadDataError::WrongDescriptor(type_desc.clone()))
@@ -106,6 +100,7 @@ fn check_for_extra_bytes<R: io::Read>(reader: &mut R) -> Result<(), ReadDataErro
 
 pub type UvwArray = Array2<Uvw>;
 
+#[derive(Debug, PartialEq, Eq)]
 pub struct Metadata {
     pub baseline: u32,
     pub time_index: u32,
@@ -115,7 +110,50 @@ pub struct Metadata {
     pub coordinate: Coordinate,
 }
 
-#[derive(Clone, Copy)]
+impl ReadableElement for Metadata {
+    fn read_to_end_exact_vec<R: io::Read>(
+        mut reader: R,
+        type_desc: &Value,
+        len: usize,
+    ) -> Result<Vec<Self>, ReadDataError> {
+        check_type_desc(
+            type_desc,
+            "[('baseline', '<i4'), ('time_index', '<i4'), ('nr_timesteps', '<i4'), ('channel_begin', '<i4'), ('channel_end', '<i4'), ('coordinate', [('x', '<i4'), ('y', '<i4'), ('z', '<i4')])]",
+        )?;
+
+        let mut out = Vec::with_capacity(len);
+
+        for _ in 0..len {
+            let baseline = reader.read_u32::<LittleEndian>()?;
+            let time_index = reader.read_u32::<LittleEndian>()?;
+            let timestep_count = reader.read_u32::<LittleEndian>()?;
+            let channel_begin = reader.read_u32::<LittleEndian>()?;
+            let channel_end = reader.read_u32::<LittleEndian>()?;
+            let coordinate_x = reader.read_u32::<LittleEndian>()?;
+            let coordinate_y = reader.read_u32::<LittleEndian>()?;
+            let coordinate_z = reader.read_u32::<LittleEndian>()?;
+
+            out.push(Metadata {
+                baseline,
+                time_index,
+                timestep_count,
+                channel_begin,
+                channel_end,
+                coordinate: Coordinate {
+                    x: coordinate_x,
+                    y: coordinate_y,
+                    z: coordinate_z,
+                },
+            });
+        }
+
+        check_for_extra_bytes(&mut reader)?;
+
+        Ok(out)
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Coordinate {
     pub x: u32,
     pub y: u32,
