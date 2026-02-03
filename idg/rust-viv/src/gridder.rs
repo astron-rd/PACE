88d9f54f340d2b2ab50b1afabdbe0e40ebb87cb8
Vivian <me@vivstuff.gay>
2026-02-03 11:01:27 +0100

    grid_onto_subgrids

--- idg/rust-viv/src/gridder.rs
+++ idg/rust-viv/src/gridder.rs
@@ -1,3 +1,13 @@
+use std::f32::consts::PI;
+
+use ndarray::prelude::*;
+use num_complex::Complex32;
+
+use crate::{
+    constants::{NR_CORRELATIONS_IN, NR_CORRELATIONS_OUT},
+    types::{Metadata, Uvw, UvwArray},
+};
+
 pub struct Gridder {
     nr_correlations_in: u32,
     nr_correlations_out: u32,
@@ -12,4 +22,168 @@ impl Gridder {
             subgrid_size,
         }
     }
+
+    pub fn grid_onto_subgrids(
+        &self,
+        w_step: f32,
+        image_size: f32,
+        grid_size: u32,
+        wavenumbers: &Array1<f32>,
+        uvw: &UvwArray,
+        visibilities: &Array4<Complex32>,
+        taper: &Array2<f32>,
+        metadata: &Array1<Metadata>,
+        subgrids: &mut Array4<Complex32>,
+    ) {
+        assert_eq!(self.nr_correlations_in as usize, visibilities.shape()[3]);
+        assert_eq!(self.nr_correlations_out as usize, subgrids.shape()[1]);
+        assert_eq!(self.subgrid_size as usize, subgrids.shape()[2]);
+
+        visibilities_to_subgrids(
+            w_step,
+            image_size,
+            grid_size,
+            wavenumbers,
+            uvw,
+            visibilities,
+            taper,
+            metadata,
+            subgrids,
+        );
+    }
+}
+
+fn visibilities_to_subgrids(
+    w_step: f32,
+    image_size: f32,
+    grid_size: u32,
+    wavenumbers: &Array1<f32>,
+    uvw: &UvwArray,
+    visibilities: &Array4<Complex32>,
+    taper: &Array2<f32>,
+    metadata: &Array1<Metadata>,
+    subgrids: &mut Array4<Complex32>,
+) {
+    let subgrid_count = metadata.shape()[0];
+
+    for s in 0..subgrid_count {
+        visibilities_to_subgrid(
+            &metadata[s],
+            w_step,
+            image_size,
+            grid_size,
+            subgrids.shape()[2] as u32,
+            wavenumbers,
+            uvw,
+            visibilities,
+            taper,
+            subgrids.slice_mut(s![s as usize, .., .., ..]),
+        )
+    }
+}
+
+fn visibilities_to_subgrid(
+    metadata: &Metadata,
+    w_step: f32,
+    image_size: f32,
+    grid_size: u32,
+    subgrid_size: u32,
+    wavenumbers: &Array1<f32>,
+    uvw: &UvwArray,
+    visibilities: &Array4<Complex32>,
+    taper: &Array2<f32>,
+    mut subgrid: ArrayViewMut3<Complex32>,
+) {
+    let w_offset_in_lambda = w_step * (metadata.coordinate.z as f32 + 0.5);
+
+    let u_offset =
+        (metadata.coordinate.x as f32 + subgrid_size as f32 / 2.0 - grid_size as f32 / 2.0) * (2.0 * PI / image_size);
+    let v_offset =
+        (metadata.coordinate.y as f32 + subgrid_size as f32 / 2.0 - grid_size as f32 / 2.0) * (2.0 * PI / image_size);
+    let w_offset = 2.0 * PI * w_offset_in_lambda;
+
+    for y in 0..subgrid_size {
+        for x in 0..subgrid_size {
+            let l = compute_lm(x, subgrid_size, image_size);
+            let m = compute_lm(y, subgrid_size, image_size);
+            let n = compute_n(l, m);
+
+            let pixels = compute_pixels(
+                metadata.timestep_count,
+                metadata.time_index,
+                uvw,
+                metadata.baseline,
+                l,
+                m,
+                n,
+                u_offset,
+                v_offset,
+                w_offset,
+                metadata.channel_begin,
+                metadata.channel_end,
+                wavenumbers,
+                visibilities
+            );
+
+            let sph = taper[(y as usize, x as usize)];
+            let x_dst = (x + (subgrid_size / 2)) % subgrid_size;
+            let y_dst = (y + (subgrid_size / 2)) % subgrid_size;
+
+            for pol in 0..NR_CORRELATIONS_OUT {
+                subgrid[(pol as usize, y_dst as usize, x_dst as usize)] = pixels[pol as usize] * sph
+            }
+        }
+    }
+}
+
+fn compute_lm(x: u32, subgrid_size: u32, image_size: f32) -> f32 {
+    (x as f32 + 0.5 - (subgrid_size as f32 / 2.0)) * image_size / subgrid_size as f32
+}
+
+fn compute_n(l: f32, m: f32) -> f32 {
+    let tmp = l * l + m * m;
+
+    if tmp >= 1.0 {
+        return 1.0;
+    }
+
+    tmp / (1.0 + f32::sqrt(1.0 - tmp))
+}
+
+fn compute_pixels(
+    timestep_count: u32,
+    offset: u32,
+    uvw: &UvwArray,
+    baseline: u32,
+    l: f32,
+    m: f32,
+    n: f32,
+    u_offset: f32,
+    v_offset: f32,
+    w_offset: f32,
+    channel_begin: u32,
+    channel_end: u32,
+    wavenumbers: &Array1<f32>,
+    visibilities: &Array4<Complex32>,
+) -> Array1<Complex32> {
+    let mut pixels: Array1<Complex32> = Array1::zeros(NR_CORRELATIONS_OUT as usize);
+
+    for time in 0..timestep_count {
+        let idx = offset + time;
+        let Uvw{u, v, w} = uvw[(baseline as usize, idx as usize)];
+
+        let phase_index = u * l + v * m + w * n;
+        let phase_offset = u_offset * l + v_offset * m + w_offset * n;
+
+        for channel in channel_begin..channel_end {
+            let phase = phase_offset - (phase_index * wavenumbers[channel as usize]);
+            let phasor = (Complex32::i() * phase).exp();
+
+            for pol in 0..NR_CORRELATIONS_IN {
+                pixels[(pol % NR_CORRELATIONS_OUT) as usize] += visibilities[(baseline as usize, idx as usize, channel as usize, pol as usize)] * phasor 
+            }
+        }
+    }
+
+    pixels
 }
--- idg/rust-viv/src/main.rs
+++ idg/rust-viv/src/main.rs
@@ -1,13 +1,11 @@
+use std::f32::consts::PI;
+
 use clap::Parser;
-use ndarray::{Array1, Array2, Array4};
+use ndarray::{Array1, Array2, Array3, Array4};
 use num_complex::Complex32;
 
 use crate::{
-    cli::Cli,
-    constants::{NR_CORRELATIONS_IN, NR_CORRELATIONS_OUT},
-    init::*,
-    types::{Metadata, UvwArray},
-    util::{print_header, print_param},
+    cli::Cli, constants::{NR_CORRELATIONS_IN, NR_CORRELATIONS_OUT, SPEED_OF_LIGHT, W_STEP}, gridder::Gridder, init::*, types::{Metadata, UvwArray}, util::{print_header, print_param}
 };
 
 mod cli;
@@ -23,19 +21,30 @@ fn main() {
     print_parameters(&cli);
 
     print_header!("INITIALIZATION");
-    let uvw = generate_uvw(
-        cli.timestep_count(),
-        cli.baseline_count(),
-        cli.grid_size,
-        cli.ellipticity,
-        cli.random_seed,
-    );
 
-    ndarray_npy::write_npy("uvw.npy", &uvw).unwrap();
-    
-    let metadata = generate_metadata(cli.channel_count, cli.subgrid_size, cli.grid_size, &uvw, None);
+    let uvw: UvwArray = ndarray_npy::read_npy("../../data/uvw.npy").unwrap();
+    let frequencies64: Array1<f64> = ndarray_npy::read_npy("../../data/frequencies.npy").unwrap();
+    let frequencies: Array1<f32> = frequencies64.mapv(|x| x as f32);
+    let wavenumbers = (frequencies * 2.0 * PI) / SPEED_OF_LIGHT;
+    let metadata: Array1<Metadata> = ndarray_npy::read_npy("../../data/metadata.npy").unwrap();
+    let subgrid_count = metadata.len();
+    let visibilities: Array4<Complex32> = ndarray_npy::read_npy("../../data/visibilities.npy").unwrap();
+    let grid: Array3<Complex32> = Array3::zeros((
+        NR_CORRELATIONS_OUT as usize,
+        cli.grid_size as usize,
+        cli.grid_size as usize,
+    ));
+    let taper: Array2<f32> = ndarray_npy::read_npy("../../data/taper.npy").unwrap();
+    let mut subgrids: Array4<Complex32> = Array4::zeros((
+        subgrid_count as usize,
+        NR_CORRELATIONS_OUT as usize,
+        cli.subgrid_size as usize,
+        cli.subgrid_size as usize,
+    ));
 
-    ndarray_npy::write_npy("metadata.npy", &metadata).unwrap();
+    print_header!("MAIN");
+    let gridder = Gridder::new(NR_CORRELATIONS_IN, cli.subgrid_size);
+    gridder.grid_onto_subgrids(W_STEP, cli.image_size(), cli.grid_size, &wavenumbers, &uvw, &visibilities, &taper, &metadata, &mut subgrids);
 }
 
 fn print_parameters(cli: &Cli) {
