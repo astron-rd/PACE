7de21cd1dfb1f6a20a6d274d9d85e5b3816a6ce8
Vivian <me@vivstuff.gay>
2026-02-12 15:05:39 +0100

    fix grid being made with subgrid size

--- idg/rust/src/types/grid.rs
+++ idg/rust/src/types/grid.rs
@@ -13,8 +13,8 @@ impl GridExtension for Grid {
     fn initialize(cli: &Cli) -> Self {
         Array3::zeros((
             NR_CORRELATIONS_OUT as usize,
-            cli.subgrid_size as usize,
-            cli.subgrid_size as usize,
+            cli.grid_size as usize,
+            cli.grid_size as usize,
         ))
     }
 }
