20b7dfc9dd6226188f002f6a3c854b2c6939b913
lukken <lukken@astron.nl>
2026-02-16 09:25:32 +0100

    Remove license headers

--- idg/python/tests/test_noop.py
+++ idg/python/tests/test_noop.py
@@ -1,7 +1,3 @@
-#  Copyright (C) 2026 ASTRON (Netherlands Institute for Radio Astronomy)
-#  Copyright (C) 2026 Researchable
-#  SPDX-License-Identifier: Apache-2.0
-
 """Testing example / placeholder"""
 
 from unittest import TestCase
