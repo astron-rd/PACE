06a211cf1da24c3eca461d2cebf7b2b48204d069
b.haaksema <b.haaksema@researchable.nl>
2026-02-25 17:09:06 +0100

    feat: save arrays into a single file

--- idg/input/src/main.py
+++ idg/input/src/main.py
@@ -1,41 +1,44 @@
+import numpy as np
+
 import initializer
 from config import settings
-from serializer import Serializer
 
 
 def main():
-    targets = {}
-
-    targets["uvw"] = initializer.init_uvw(
+    uvw = initializer.init_uvw(
         observation_hours=settings.observation_hours,
         nr_baselines=settings.nr_baselines,
         grid_size=settings.grid_size,
     )
-    targets["frequencies"] = initializer.init_frequencies(
+    frequencies = initializer.init_frequencies(
         start_frequency=settings.start_frequency,
         frequency_increment=settings.frequency_increment,
         nr_channels=settings.nr_channels,
     )
-    targets["metadata"] = initializer.init_metadata(
+    metadata = initializer.init_metadata(
         nr_channels=settings.nr_channels,
         subgrid_size=settings.subgrid_size,
         grid_size=settings.grid_size,
-        uvw=targets["uvw"],
+        uvw=uvw,
     )
-    targets["visibilities"] = initializer.init_visibilities(
+    visibilities = initializer.init_visibilities(
         nr_correlations=settings.nr_correlations_in,
         nr_channels=settings.nr_channels,
         nr_timesteps=settings.nr_timesteps,
         nr_baselines=settings.nr_baselines,
         image_size=settings.image_size,
         grid_size=settings.grid_size,
-        frequencies=targets["frequencies"],
-        uvw=targets["uvw"],
+        frequencies=frequencies,
+        uvw=uvw,
     )
 
-    serializer = Serializer()
-    for name, array in targets.items():
-        serializer.save(name, array)
+    np.savez(
+        "artifact.npz",
+        uvw=uvw,
+        frequencies=frequencies,
+        metadata=metadata,
+        visibilities=visibilities,
+    )
 
 
 if __name__ == "__main__":
