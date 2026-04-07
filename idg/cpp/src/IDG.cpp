508a533a64b37db070ab5ae72534af79f90b5713
Vivian <me@vivstuff.gay>
2026-04-07 18:30:46 +0200

    correct correct fft

--- idg/cpp/src/IDG.cpp
+++ idg/cpp/src/IDG.cpp
@@ -23,7 +23,7 @@ void Gridder::grid_onto_subgrids(
   assert(subgrid_size_ == subgrids.shape()[2]);
   const size_t nr_subgrids = metadata.size();
 
-#pragma omp parallel for schedule(dynamic)
+  // #pragma omp parallel for schedule(dynamic)
   for (size_t s = 0; s < nr_subgrids; ++s) {
     auto subgrid =
         xt::eval(xt::view(subgrids, s, xt::all(), xt::all(), xt::all()));
@@ -38,14 +38,14 @@ void Gridder::grid_onto_subgrids(
 
 void Gridder::ifft_subgrids(xt::xarray<std::complex<float>> &subgrids) const {
   for (size_t s = 0; s < subgrids.shape(0); ++s) {
-    for (size_t c = 0; c < subgrids.shape(1); ++s) {
+    for (size_t c = 0; c < subgrids.shape(1); ++c) {
       auto subgrid = xt::eval(xt::view(subgrids, s, c, xt::all(), xt::all()));
 
       subgrid = xt::fftw::ifft2(subgrid);
 
       subgrid = subgrid / (subgrid_size_ * subgrid_size_);
 
-      xt::view(subgrids, s, xt::all(), xt::all(), xt::all()) = subgrid;
+      xt::view(subgrids, s, c, xt::all(), xt::all()) = subgrid;
     }
   }
 }
