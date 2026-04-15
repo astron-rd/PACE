6958d15e4a2b6dc6566997f8ba6452330327f4ad
Bas Haaksema <17254355+bhaaksema@users.noreply.github.com>
2026-04-15 16:45:06 +0200

    fix: update benchmark to use warmup round

--- idg/python/tests/bench_kernels.py
+++ idg/python/tests/bench_kernels.py
@@ -7,4 +7,4 @@ from idg_python.kernels.numba import evaluate_spheroidal
 
 def test_evaluate_spheroidal(benchmark):
     nu = np.linspace(0.0, 1.0, 1024)
-    benchmark(evaluate_spheroidal, nu)
+    benchmark.pedantic(evaluate_spheroidal, args=(nu,), warmup_rounds=1)
