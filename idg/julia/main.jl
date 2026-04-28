4f240ada685191b78c58e23b8dab5f4b27cb2c4c
Vivian <me@vivstuff.gay>
2026-04-28 10:18:27 +0200

    load data from hdf5

--- idg/julia/main.jl
+++ idg/julia/main.jl
@@ -1,6 +1,15 @@
 module IDG
 
-using NPZ
-uvws = npzread("input/uvw.npy")
+using HDF5
+
+fid = HDF5.h5open("input/input.hdf5", "r")
+
+uvw_dataset = fid["uvws"]
+uvws = read(uvw_dataset)
+
+metadata_dataset = fid["metadata"]
+metadata = read(metadata_dataset)
+
+close(fid)
 
 end
