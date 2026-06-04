eeedbbb9c2e831f6ffb0b2169661dd5bee25552a
Bastiaan Haaksema <b.haaksema@researchable.nl>
2026-06-04 15:13:55 +0200

    chore: remove duplicated idg python input generation

--- idg/python/idg_python/main.py
+++ idg/python/idg_python/main.py
@@ -6,56 +6,24 @@ import h5py
 import numpy as np
 
 from idg_python import idgtypes
-from idg_python.idg import Gridder  # type: ignore
-from idg_python.init import (
-    get_frequencies,
-    get_metadata,
-    get_taper,
-    get_uvw,
-    get_visibilities,
-)
+from idg_python.idg import Gridder
+from idg_python.init import get_taper
 
 # Dictionary to store all timings
 timings = {}
 
 # Constants
-NR_CORRELATIONS_IN = 2  # XX, YY
 NR_CORRELATIONS_OUT = 1  # I
 W_STEP = 1.0  # w step in wavelengths
 SPEED_OF_LIGHT = 299792458.0
-START_FREQUENCY = 150e6  # 150 MHz
-FREQUENCY_INCREMENT = 1e6  # 1 MHz
 
 
 def main():
     # Command line arguments
     parser = argparse.ArgumentParser()
+    parser.add_argument("input", help="Input HDF5 file")
     parser.add_argument(
-        "--subgrid_size", type=int, default=32, help="Size of the subgrid in pixels"
-    )
-    parser.add_argument(
-        "--grid_size", type=int, default=1024, help="Size of the grid in pixels"
-    )
-    parser.add_argument(
-        "--observation_hours",
-        type=float,
-        default=4,
-        help="Length of the observation in hours",
-    )
-    parser.add_argument(
-        "--nr_channels", type=int, default=16, help="Number of frequency channels"
-    )
-    parser.add_argument(
-        "--nr_stations", type=int, default=20, help="Number of stations"
-    )
-    parser.add_argument(
-        "--load",
-        const="input.h5",
-        nargs="?",
-        help="Load input data from HDF5 file (optional: specify filename)",
-    )
-    parser.add_argument(
-        "--store", action="store_true", default=False, help="Store data in Numpy format"
+        "--store", action="store_true", default=False, help="Store data in HDF5 format"
     )
     parser.add_argument(
         "--json",
@@ -65,42 +33,10 @@ def main():
     )
     args = parser.parse_args()
 
-    SUBGRID_SIZE = args.subgrid_size
-    GRID_SIZE = args.grid_size
-    OBSERVATION_HOURS = args.observation_hours
-    NR_CHANNELS = args.nr_channels
-    LOAD_INPUT = args.load
-    STORE_DATA = args.store
-    OUTPUT_FILENAME = args.json
-
-    # Derived arguments
-    NR_TIMESTEPS = int(OBSERVATION_HOURS * 3600)
-    END_FREQUENCY = START_FREQUENCY + (NR_CHANNELS - 1) * FREQUENCY_INCREMENT
-    IMAGE_SIZE = SPEED_OF_LIGHT / END_FREQUENCY
-    NR_STATIONS = args.nr_stations
-    NR_BASELINES = NR_STATIONS * (NR_STATIONS - 1) // 2
-
     def print_header(title, header_length=50, newline="\n"):
         print(newline + "=" * header_length)
         print(title)
         print("=" * header_length)
-
-    parameters = {
-        "nr_correlations_in": NR_CORRELATIONS_IN,
-        "nr_correlations_out": NR_CORRELATIONS_OUT,
-        "start_frequency": START_FREQUENCY * 1e-6,
-        "frequency_increment": FREQUENCY_INCREMENT * 1e-6,
-        "nr_channels": NR_CHANNELS,
-        "nr_timesteps": NR_TIMESTEPS,
-        "nr_stations": NR_STATIONS,
-        "nr_baselines": NR_BASELINES,
-        "subgrid_size": SUBGRID_SIZE,
-        "grid_size": GRID_SIZE,
-    }
-
-    print_header("PARAMETERS", newline="")
-    for key, value in parameters.items():
-        print(f"{key:<39} {value:>10}")
 
     def timeit(description, operation):
         print(f"{description:<38}", end="")
@@ -112,93 +48,70 @@ def main():
         timings[description] = duration
         return result
 
-    if LOAD_INPUT is not None:
-        print_header("READING INPUT DATA")
-        with h5py.File(LOAD_INPUT, "r") as infile:
-            uvw_ds = infile["uvws"]
-            if not isinstance(uvw_ds, h5py.Dataset):
-                print("Invalid input file: uvw is not defined")
-                return
-            uvw = timeit("Load UVW coordinates", lambda: uvw_ds[...])
+    print_header("READING INPUT DATA", newline="")
+    with h5py.File(args.input, "r") as infile:
+        grid_size = int(infile.attrs["grid_size"])
+        subgrid_size = int(infile.attrs["subgrid_size"])
 
-            frequencies_ds = infile["frequencies"]
-            if not isinstance(frequencies_ds, h5py.Dataset):
-                print("Invalid input file: frequencies is not defined")
-                return
-            frequencies = timeit("Load frequencies", lambda: frequencies_ds[...])
-            wavenumbers = (frequencies * 2 * np.pi) / SPEED_OF_LIGHT
+        uvw_ds = infile["uvws"]
+        if not isinstance(uvw_ds, h5py.Dataset):
+            print("Invalid input file: uvws is not defined")
+            return
+        uvw = timeit("Load UVW coordinates", lambda: uvw_ds[...])
 
-            metadata_ds = infile["metadata"]
-            if not isinstance(metadata_ds, h5py.Dataset):
-                print("Invalid input file: frequencies is not defined")
-                return
-            metadata = timeit("Load metadata", lambda: metadata_ds[...])
-            nr_subgrids = metadata.shape[0]
+        frequencies_ds = infile["frequencies"]
+        if not isinstance(frequencies_ds, h5py.Dataset):
+            print("Invalid input file: frequencies is not defined")
+            return
+        frequencies = timeit("Load frequencies", lambda: frequencies_ds[...])
 
-            visibilities_ds = infile["visibilities"]
-            if not isinstance(visibilities_ds, h5py.Dataset):
-                print("Invalid input file: frequencies is not defined")
-                return
-            visibilities = timeit("Load visibilities", lambda: visibilities_ds[...])
-
-    else:
-        print_header("GENERATING INPUT DATA")
-        uvw = timeit(
-            "Initialize UVW coordinates",
-            lambda: get_uvw(
-                observation_hours=OBSERVATION_HOURS,
-                nr_baselines=NR_BASELINES,
-                grid_size=GRID_SIZE,
-            ),
-        )
-
-        frequencies = timeit(
-            "Initialize frequencies",
-            lambda: get_frequencies(START_FREQUENCY, FREQUENCY_INCREMENT, NR_CHANNELS),
-        )
-        wavenumbers = (frequencies * 2 * np.pi) / SPEED_OF_LIGHT
-
-        metadata = timeit(
-            "Initialize metadata",
-            lambda: get_metadata(
-                nr_channels=NR_CHANNELS,
-                subgrid_size=SUBGRID_SIZE,
-                grid_size=GRID_SIZE,
-                uvw=uvw,
-            ),
-        )
+        metadata_ds = infile["metadata"]
+        if not isinstance(metadata_ds, h5py.Dataset):
+            print("Invalid input file: metadata is not defined")
+            return
+        metadata = timeit("Load metadata", lambda: metadata_ds[...])
         nr_subgrids = metadata.shape[0]
 
-        visibilities = timeit(
-            "Initialize visibilities",
-            lambda: get_visibilities(
-                nr_correlations=NR_CORRELATIONS_IN,
-                nr_channels=NR_CHANNELS,
-                nr_timesteps=NR_TIMESTEPS,
-                nr_baselines=NR_BASELINES,
-                image_size=IMAGE_SIZE,
-                grid_size=GRID_SIZE,
-                frequencies=frequencies,
-                uvw=uvw,
-            ),
-        )
+        visibilities_ds = infile["visibilities"]
+        if not isinstance(visibilities_ds, h5py.Dataset):
+            print("Invalid input file: visibilities is not defined")
+            return
+        visibilities = timeit("Load visibilities", lambda: visibilities_ds[...])
+
+    wavenumbers = (frequencies * 2 * np.pi) / SPEED_OF_LIGHT
+    image_size = SPEED_OF_LIGHT / frequencies[-1]
+    nr_correlations_in = visibilities.shape[-1]
+
+    parameters = {
+        "nr_correlations_in": nr_correlations_in,
+        "nr_correlations_out": NR_CORRELATIONS_OUT,
+        "nr_channels": len(frequencies),
+        "nr_timesteps": uvw.shape[1],
+        "nr_baselines": uvw.shape[0],
+        "subgrid_size": subgrid_size,
+        "grid_size": grid_size,
+    }
+
+    print_header("PARAMETERS")
+    for key, value in parameters.items():
+        print(f"{key:<39} {value:>10}")
 
     grid = np.zeros(
-        (NR_CORRELATIONS_OUT, GRID_SIZE, GRID_SIZE), dtype=idgtypes.gridtype
+        (NR_CORRELATIONS_OUT, grid_size, grid_size), dtype=idgtypes.gridtype
     )
 
-    taper = timeit("Initialize taper", lambda: get_taper(subgrid_size=SUBGRID_SIZE))
+    taper = timeit("Initialize taper", lambda: get_taper(subgrid_size=subgrid_size))
 
     subgrids = np.zeros(
-        shape=(nr_subgrids, NR_CORRELATIONS_OUT, SUBGRID_SIZE, SUBGRID_SIZE),
+        shape=(nr_subgrids, NR_CORRELATIONS_OUT, subgrid_size, subgrid_size),
         dtype=idgtypes.gridtype,
     )
 
     gridder = timeit(
         "Initialize gridder",
         lambda: Gridder(
-            nr_correlations_in=NR_CORRELATIONS_IN,
-            subgrid_size=SUBGRID_SIZE,
+            nr_correlations_in=nr_correlations_in,
+            subgrid_size=subgrid_size,
         ),
     )
 
@@ -208,8 +121,8 @@ def main():
         "Grid visibilities",
         lambda: gridder.grid_onto_subgrids(
             w_step=W_STEP,
-            image_size=IMAGE_SIZE,
-            grid_size=GRID_SIZE,
+            image_size=image_size,
+            grid_size=grid_size,
             wavenumbers=wavenumbers,
             uvw=uvw,
             visibilities=visibilities,
@@ -222,17 +135,14 @@ def main():
     timeit(
         "Add subgrids",
         lambda: gridder.add_subgrids_to_grid(
-            metadata=metadata,
-            subgrids=subgrids,
-            grid=grid,
+            metadata=metadata, subgrids=subgrids, grid=grid
         ),
     )
 
     timeit(
         "Transform grid",
         lambda: gridder.transform(
-            direction=idgtypes.FOURIER_DOMAIN_TO_IMAGE_DOMAIN,
-            grid=grid,
+            direction=idgtypes.FOURIER_DOMAIN_TO_IMAGE_DOMAIN, grid=grid
         ),
     )
 
@@ -243,26 +153,21 @@ def main():
         print(f"{operation:<30} {duration:>8.3f} s ({percentage:>5.1f}%)")
     print(f"{'Total':<30} {total_time:>8.3f} s")
 
-    if STORE_DATA:
+    if args.store:
         output_file = h5py.File("output.h5", "w")
         output_file.create_dataset("grid", data=grid)
         output_file.create_dataset("subgrids", data=subgrids)
 
-    if OUTPUT_FILENAME:
-        output = {
-            "parameters": {},
-            "timings": {},
-        }
+    if args.json:
+        output = {"parameters": {}, "timings": {}}
 
-        for parameter in parameters.items():
-            key, value = parameter
+        for key, value in parameters.items():
             output["parameters"][key] = value
 
-        for timing in timings.items():
-            operation, duration = timing
+        for operation, duration in timings.items():
             output["timings"][operation.lower().replace(" ", "_")] = round(
                 duration * 1000, 2
             )
 
-        with open(OUTPUT_FILENAME, "w") as f:
+        with open(args.json, "w") as f:
             json.dump(output, f, indent=2)
