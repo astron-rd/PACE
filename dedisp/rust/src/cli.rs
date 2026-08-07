9bfb89fdf4b4c13f520200964b93acc2f7f36316
Vivian <me@vivstuff.gay>
2026-08-07 14:24:38 +0200

    Init

---
+++ dedisp/rust/src/cli.rs
@@ -0,0 +1,7 @@
+use clap::Parser;
+
+#[derive(Parser)]
+#[command(version, about, long_about = Some("Fourier Domain Dedispersion"))]
+pub struct Cli {
+    
+}
---
+++ dedisp/rust/src/main.rs
@@ -0,0 +1,7 @@
+use clap::Parser;
+
+mod cli;
+
+fn main() {
+    let cli = cli::Cli::parse();
+}
