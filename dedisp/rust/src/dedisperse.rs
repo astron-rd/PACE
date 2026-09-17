0d84e5da5e6bccd82f5959b42af212b9d3a282c1
Vivian <me@vivstuff.gay>
2026-09-17 10:25:03 +0200

    dedispering

---
+++ dedisp/rust/src/dedisperse.rs
@@ -0,0 +1,220 @@
+use std::f32::consts::PI;
+
+use ndarray::{Array1, Array2, Axis, s};
+use ndarray_ndimage::PadMode::Constant;
+use ndrustfft::{Complex, R2cFftHandler};
+
+use crate::cli::{DedispArgs, GeneralArgs, ObservationArgs};
+use crate::util::time_function;
+
+pub fn dedisperse(
+    general_args: &GeneralArgs,
+    observation_args: &ObservationArgs,
+    dedisp_args: &DedispArgs,
+) {
+    let signal_file = hdf5_metno::File::open(&general_args.signal_file).unwrap();
+    let signal: Array2<u8> = time_function!(
+        "read input",
+        signal_file
+            .dataset("dynspec")
+            .expect("dynspec dataset should be in input file")
+            .read()
+            .unwrap()
+    );
+
+    let frequency_resolution =
+        -1.0 * observation_args.bandwidth / observation_args.channel_count as f32;
+    let mut plan = time_function!(
+        "create FDDPlan",
+        FDDPlan::new(
+            observation_args.channel_count,
+            observation_args.sampling_period,
+            observation_args.max_frequency,
+            frequency_resolution,
+        )
+    );
+
+    time_function!(
+        "generate dm list",
+        plan.generate_dm_list(
+            dedisp_args.dm_start,
+            dedisp_args.dm_end,
+            dedisp_args.pulse_width,
+            dedisp_args.tolerance
+        )
+    );
+
+    let output = time_function!("execute fdd plan", plan.execute(signal));
+
+    dbg!(output);
+}
+
+pub struct FDDPlan {
+    dm_count: usize,
+    channel_count: usize,
+    max_delay: usize,
+
+    time_resolution: f32,
+    max_frequency: f32,
+    frequency_resolution: f32,
+
+    dm_table: Array1<f32>,
+    delay_table: Array1<f32>,
+}
+
+impl FDDPlan {
+    fn new(
+        channel_count: usize,
+        time_resolution: f32,
+        max_frequency: f32,
+        frequency_resolution: f32,
+    ) -> Self {
+        let mut zelf = Self {
+            dm_count: 0,
+            channel_count,
+            max_delay: 0,
+            time_resolution,
+            max_frequency,
+            frequency_resolution,
+            dm_table: Default::default(),
+            delay_table: Default::default(),
+        };
+
+        zelf.generate_delay_table();
+
+        zelf
+    }
+
+    fn generate_delay_table(&mut self) {
+        const MYSTERIOUS_MAGIC_CONSTANT: f32 = 4.148741601e3;
+
+        self.delay_table = Array1::from_iter((0..self.channel_count).map(|channel| {
+            let inverse_channel_frequency =
+                1.0 / (self.max_frequency + channel as f32 * self.frequency_resolution);
+            let inverse_max_frequency = 1.0 / self.max_frequency;
+
+            MYSTERIOUS_MAGIC_CONSTANT / self.time_resolution
+                * (inverse_channel_frequency.powi(2) - inverse_max_frequency.powi(2))
+        }));
+    }
+
+    fn generate_dm_list(&mut self, dm_start: f32, dm_end: f32, pulse_width: f32, tolerance: f32) {
+        let time_resolution = self.time_resolution as f64 * 1e6;
+        let f = (self.max_frequency as f64
+            + ((self.channel_count / 2) as f64 - 0.5) * self.frequency_resolution as f64)
+            * 1e-3;
+        let a = 8.3 * self.frequency_resolution as f64 / f.powi(3);
+        let a_squared = a.powi(2);
+        let b_squared = a_squared * (self.channel_count.pow(2) / 16) as f64;
+        let tolerance_squared = (tolerance as f64).powi(2);
+        let c =
+            (time_resolution.powi(2) + (pulse_width as f64).powi(2)) * (tolerance_squared - 1.0);
+
+        let mut dm_table = vec![dm_start];
+        while *dm_table.last().unwrap() < dm_end {
+            let previous_dm = *dm_table.last().unwrap() as f64;
+            let previous_dm_squared = previous_dm.powi(2);
+            let k = c + tolerance_squared * a_squared * previous_dm_squared;
+            let dm = (b_squared * previous_dm
+                + (-a_squared * b_squared * previous_dm_squared + (a_squared + b_squared) * k)
+                    .sqrt())
+                / (a_squared + b_squared);
+            dm_table.push(dm as f32);
+        }
+
+        self.dm_table = Array1::from_vec(dm_table);
+    }
+
+    fn execute(&self, spectrum: Array2<u8>) -> Array2<f32> {
+        let n_samples = spectrum.shape()[0];
+        let n_spin_frequencies = n_samples / 2 + 1;
+        let n_output_samples = n_samples - self.max_delay;
+
+        let n_samples_fft = (n_samples + 1).next_multiple_of(16384);
+        let n_samples_padded = (n_samples_fft).next_multiple_of(1024);
+        let n_fft_frequency_bins = n_samples_padded / 2 + 1;
+
+        println!("(1) Generate the spin frequency table.");
+
+        let spin_frequency_table: Array1<f32> = (0..n_spin_frequencies)
+            .into_iter()
+            .map(|i| i as f32 / (n_samples as f32 * self.time_resolution))
+            .collect();
+
+        println!("(2) Transpose data: int -> float.");
+
+        let padding = n_samples_padded - n_samples;
+        let padded_input = ndarray_ndimage::pad(&spectrum, &[[0, padding], [0, 0]], Constant(0));
+        let mut transposed_input = padded_input.map(|x| *x as f32);
+        transposed_input -= 127.5;
+        transposed_input /= self.channel_count as f32;
+
+        let mut fd_scratch = Array2::zeros((n_fft_frequency_bins, self.channel_count));
+        time_function!(
+            "fft real->complex",
+            ndrustfft::ndfft_r2c_par(
+                &transposed_input,
+                &mut fd_scratch,
+                &R2cFftHandler::new(transposed_input.shape()[0]),
+                0,
+            )
+        );
+
+        let mut dm_scratch: Array2<Complex<f32>> =
+            Array2::zeros((self.dm_count, fd_scratch.shape()[1]));
+
+        time_function!(
+            "dedisperse",
+            Self::fourier_domain_dedispersion(
+                &fd_scratch,
+                &mut dm_scratch,
+                self.time_resolution,
+                &spin_frequency_table,
+                &self.dm_table,
+                &self.delay_table,
+            )
+        );
+
+        let mut dm_output = Array2::zeros((self.dm_count, n_samples_padded));
+        ndrustfft::ndifft_r2c_par(
+            &dm_scratch,
+            &mut dm_output,
+            &R2cFftHandler::new(n_samples_padded),
+            1,
+        );
+
+        dm_output.slice(s![.., ..n_output_samples]).to_owned()
+    }
+
+    fn fourier_domain_dedispersion(
+        input_data: &Array2<Complex<f32>>,
+        output_data: &mut Array2<Complex<f32>>,
+        time_resolution: f32,
+        spin_frequencies: &Array1<f32>,
+        dispersion_measures: &Array1<f32>,
+        delays: &Array1<f32>,
+    ) {
+        let n_spin_frequencies = spin_frequencies.len();
+        dbg!(&n_spin_frequencies);
+        dbg!(&input_data.shape());
+        let samples = input_data.slice(s![.., ..n_spin_frequencies]);
+        let spin_frequencies_matrix = spin_frequencies
+            .to_shape((1, spin_frequencies.len()))
+            .unwrap()
+            .reversed_axes();
+
+        for (idx, dm) in dispersion_measures.iter().enumerate() {
+            let dm_delays = delays.clone() * (dm * time_resolution);
+
+            let dm_delays_matrix = dm_delays.to_shape((1, dm_delays.len())).unwrap();
+
+            let phases = 2.0 * PI * (dm_delays_matrix * &spin_frequencies_matrix);
+            let phasors = phases.map(|x| Complex::new(0.0, *x).exp());
+
+            let result = (samples.to_owned() * phasors).sum_axis(Axis(0));
+            output_data
+                .slice_mut(s![idx, ..n_spin_frequencies])
+                .assign(&result);
+        }
+    }
+}
--- dedisp/rust/src/main.rs
+++ dedisp/rust/src/main.rs
@@ -1,88 +1,22 @@
-use std::f32;
-
 use clap::Parser;
-use ndarray::Array2;
-use ndarray_rand::RandomExt;
-
-use crate::{
-    metadata::{ObservationInfo, SignalInfo},
-    util::time_function,
-};
 
 mod cli;
-mod metadata;
 mod util;
 
+mod dedisperse;
+mod simulate;
+
 fn main() {
-    let _cli = cli::Cli::parse();
+    let cli = cli::Cli::parse();
 
-    let observation = ObservationInfo {
-        duration: 30.0,
-        sampling_period: 250.0e-6,
-        peak_frequency: 1581.0,
-        bandwidth: 100.0,
-        channel_count: 1024,
-    };
-
-    let signal_properties = SignalInfo {
-        noise_rms: 25.0,
-        dispersion_measure: 41.159,
-        arrival_time: f32::consts::PI,
-        intensity: 25.0,
-    };
-
-    println!("Simulating a dispersed signal...");
-
-    let signal = simulate_dispersed_signal(&signal_properties, &observation);
-
-    let quantized_signal = time_function!("quantize signal", signal.map(quantize));
-
-    time_function!("save signal to disk", {
-        let output_file = hdf5_metno::File::create("signal.h5").unwrap();
-
-        let builder = output_file.new_dataset_builder();
-        builder
-            .with_data(&quantized_signal)
-            .create("signal")
-            .unwrap();
-    });
-}
-
-fn simulate_dispersed_signal(
-    signal: &SignalInfo,
-    observation: &ObservationInfo,
-) -> ndarray::Array2<f32> {
-    let frequency_resolution = -observation.bandwidth / observation.channel_count as f32;
-    let n_samples = observation.duration / observation.sampling_period;
-
-    let shape = (n_samples as usize, observation.channel_count);
-    let mut data = time_function!(
-        "create background noise",
-        Array2::random(shape, ndarray_rand::rand_distr::StandardNormal) * signal.noise_rms
-    );
-
-    time_function!("add dispersed signal", {
-        for channel in 0..observation.channel_count {
-            let a = 1.0 / (observation.peak_frequency + channel as f32 * frequency_resolution);
-            let b = 1.0 / observation.peak_frequency;
-
-            let channel_delay = signal.dispersion_measure * 4.15e3 * (a * a - b * b);
-            let sample = (signal.arrival_time + channel_delay) / observation.sampling_period;
-
-            data[(sample as usize, channel)] += signal.intensity;
-        }
-    });
-
-    data
-}
-
-fn quantize(value_in: &f32) -> u8 {
-    let value = value_in + 127.5;
-    if value > 255.0 {
-        255
-    } else if value < 0.0 {
-        0
-    } else {
-        value.round() as u8
+    match &cli.command {
+        cli::Commands::Simulate {
+            observation_args,
+            signal_args,
+        } => simulate::simulate(&cli.args, observation_args, signal_args),
+        cli::Commands::Dedisperse {
+            observation_args,
+            dedisp_args,
+        } => dedisperse::dedisperse(&cli.args, observation_args, dedisp_args),
     }
 }
